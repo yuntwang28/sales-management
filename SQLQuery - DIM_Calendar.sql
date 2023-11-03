0a552e2ff6311536a7d2e80c4a768ab26ecc0128
unknown <yunting.wang@gmail.com>
2023-11-03 17:24:22 +1100

    Add to remote GitHib

---
+++ SQLQuery - DIM_Calendar.sql
@@ -0,0 +1,25 @@
+-- Cleansed DIM_DateTable--
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date,
+  --,[DayNumberOfWeek], 
+  [EnglishDayNameOfWeek] AS Day,
+  --,[SpanishDayNameOfWeek]
+  --,[FrenchDayNameOfWeek]
+  --,[DayNumberOfMonth]
+  --,[DayNumberOfYear], 
+  [WeekNumberOfYear] AS WeekNr, 
+  [EnglishMonthName] AS Month,
+  Left ([EnglishMonthName],3) AS MonthShort,
+  --,[SpanishMonthName]
+  --,[FrenchMonthName], 
+  [MonthNumberOfYear] AS MonthNo,
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year
+  --,[CalendarSemester]
+  --,[FiscalQuarter]
+  --,[FiscalYear]
+  --,[FiscalSemester]
+FROM 
+  [AdventureWorksDW2022].[dbo].[DimDate]
+  WHERE CalendarYear >= 2019
---
+++ SQLQuery - DIM_Customers.sql
@@ -0,0 +1,36 @@
+-- Cleansed DIM_Customers Table --
+SELECT c.customerkey AS CustomerKey,
+      --,[GeographyKey]
+      --,[CustomerAlternateKey]
+      --,[Title]
+      c.firstname AS [First Name], 
+      --,[MiddleName]
+      c.lastname AS [Last Name], 
+	  c.firstname + ' ' + lastname AS [Full Name], 
+      --,[NameStyle]
+      --,[BirthDate]
+      --,[MaritalStatus]
+      --,[Suffix]
+      CASE c.gender WHEN 'M' THEN 'Male' WHEN 'F' THEN 'Female' END AS Gender,
+      --,[EmailAddress]
+      --,[YearlyIncome]
+      --,[TotalChildren]
+      --,[NumberChildrenAtHome]
+      --,[EnglishEducation]
+      --,[SpanishEducation]
+      --,[FrenchEducation]
+      --,[EnglishOccupation]
+      --,[SpanishOccupation]
+      --,[FrenchOccupation]
+      --,[HouseOwnerFlag]
+      --,[NumberCarsOwned]
+      --,[AddressLine1]
+      --,[AddressLine2]
+      --,[Phone]
+      c.datefirstpurchase AS DateFirstPurchase, 
+      --,[CommuteDistance]
+	  g.city AS [Customer City]
+  FROM [AdventureWorksDW2022].[dbo].[DimCustomer] as c
+	  LEFT JOIN dbo.dimgeography AS g ON g.geographykey = c.geographykey 
+  ORDER BY 
+	  CustomerKey ASC -- Ordered List by CustomerKey
